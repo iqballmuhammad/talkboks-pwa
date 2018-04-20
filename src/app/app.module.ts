6e1a0a0e0e39cfe62245dfa77ab55eb762f22597
iqballmuhammad <iqbalmuhammad.18@gmail,com>
2018-04-20 18:46:44 +0700

    service worker & PWA initialization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,48 +20,6 @@ import {
 } from '@covalent/core';
 import {MainPageComponent} from './pages/main-page/main-page.component';
 import {DashboardPageComponent} from './pages/dashboard-page/dashboard-page.component';
-import {TypographyPageComponent} from './pages/typography-page/typography-page.component';
-import {ColorPageComponent} from './pages/color-page/color-page.component';
-import {ButtonsPageComponent} from './pages/buttons-page/buttons-page.component';
-import {FormElementsPageComponent} from './pages/form-elements-page/form-elements-page.component';
-import {FormWizardPageComponent} from './pages/form-wizard-page/form-wizard-page.component';
-import {ChipsPageComponent} from './pages/chips-page/chips-page.component';
-import {DialogPageComponent} from './pages/dialog-page/dialog-page.component';
-import {GridListPageComponent} from './pages/grid-list-page/grid-list-page.component';
-import {IconsPageComponent} from './pages/icons-page/icons-page.component';
-import {ListPageComponent} from './pages/list-page/list-page.component';
-import {MenuPageComponent} from './pages/menu-page/menu-page.component';
-import {ProgressBarPageComponent} from './pages/progress-bar-page/progress-bar-page.component';
-import {SpinnerPageComponent} from './pages/spinner-page/spinner-page.component';
-import {SnackbarPageComponent} from './pages/snackbar-page/snackbar-page.component';
-import {TabPageComponent} from './pages/tab-page/tab-page.component';
-import {ToolbarPageComponent} from './pages/toolbar-page/toolbar-page.component';
-import {TooltipPageComponent} from './pages/tooltip-page/tooltip-page.component';
-import {SidenavPageComponent} from './pages/sidenav-page/sidenav-page.component';
-import {FlexLayoutPageComponent} from './pages/flex-layout-page/flex-layout-page.component';
-import {CardsPageComponent} from './pages/cards-page/cards-page.component';
-import {LineChartPageComponent} from './pages/line-chart-page/line-chart-page.component';
-import {BarChartPageComponent} from './pages/bar-chart-page/bar-chart-page.component';
-import {BubbleChartPageComponent} from './pages/bubble-chart-page/bubble-chart-page.component';
-import {HeatmapPageComponent} from './pages/heatmap-page/heatmap-page.component';
-import {PieChartPageComponent} from './pages/pie-chart-page/pie-chart-page.component';
-import {RadarPageComponent} from './pages/radar-page/radar-page.component';
-import {TablePageComponent} from './pages/table-page/table-page.component';
-import {ScrollPageComponent} from './pages/scroll-page/scroll-page.component';
-import {FileUploadPageComponent} from './pages/file-upload-page/file-upload-page.component';
-import {StepperPageComponent} from './pages/stepper-page/stepper-page.component';
-import {ChatPageComponent} from './pages/chat-page/chat-page.component';
-import {TestimonialsPageComponent} from './pages/testimonials-page/testimonials-page.component';
-import {PricingTablesPageComponent} from './pages/pricing-tables-page/pricing-tables-page.component';
-import {CartButtonComponent, ProductsPageComponent} from './pages/products-page/products-page.component';
-import {ProductPageComponent} from './pages/product-page/product-page.component';
-import {CartPageComponent} from './pages/cart-page/cart-page.component';
-import {PortfolioPageComponent} from './pages/portfolio-page/portfolio-page.component';
-import {PageNotFoundComponent} from './pages/page-not-found/page-not-found.component';
-import {LoginPageComponent} from './pages/login-page/login-page.component';
-import {SignUpPageComponent} from './pages/sign-up-page/sign-up-page.component';
-import {PaletteComponent} from './pages/color-page/palette/palette.component';
-import {DialogExampleComponent} from './pages/dialog-page/dialog-example.component';
 import {TopicComponent} from './topic/topic.component';
 import {PostComponent} from './post/post.component';
 import {SidemenuModule} from './sidemenu/sidemenu.module';
@@ -81,6 +39,8 @@ import {
   MatSnackBarModule, MatTabsModule, MatToolbarModule, MatTooltipModule
 } from '@angular/material';
 import {PortalModule} from '@angular/cdk/portal';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 const PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {};
 
@@ -91,31 +51,10 @@ export function createTranslateLoader(http: HttpClient) {
 
 @NgModule({
   declarations: [
-    AppComponent, MainPageComponent, PageNotFoundComponent, DashboardPageComponent, TypographyPageComponent,
-    ColorPageComponent, PaletteComponent, ButtonsPageComponent, FormElementsPageComponent, FormWizardPageComponent,
-    ChipsPageComponent, DialogPageComponent, DialogExampleComponent, GridListPageComponent, IconsPageComponent,
-    ListPageComponent, MenuPageComponent, ProgressBarPageComponent, SpinnerPageComponent, SnackbarPageComponent,
-    TabPageComponent, ToolbarPageComponent, TooltipPageComponent, SidenavPageComponent, FlexLayoutPageComponent,
-    CardsPageComponent, LineChartPageComponent, BarChartPageComponent, BubbleChartPageComponent, HeatmapPageComponent,
-    PieChartPageComponent, RadarPageComponent,
-    TablePageComponent, ScrollPageComponent, FileUploadPageComponent,
-    StepperPageComponent,
-    LoginPageComponent,
-    SignUpPageComponent,
-    ChatPageComponent,
-    TestimonialsPageComponent,
-    PricingTablesPageComponent,
-    ProductsPageComponent,
-    CartButtonComponent,
-    CartPageComponent,
-    ProductPageComponent,
-    PortfolioPageComponent,
+    AppComponent, MainPageComponent, DashboardPageComponent, 
     MultiLanguagePageComponent,
     TopicComponent,
     PostComponent
-  ],
-  entryComponents: [
-    DialogExampleComponent, CartButtonComponent
   ],
   imports: [
     BrowserModule,
@@ -172,7 +111,8 @@ export function createTranslateLoader(http: HttpClient) {
       }
     }),
     RouterModule,
-    AppRoutesModule
+    AppRoutesModule,
+    environment.production ? ServiceWorkerModule.register('ngsw-worker.js') : []
   ],
   providers: [],
   bootstrap: [AppComponent]
